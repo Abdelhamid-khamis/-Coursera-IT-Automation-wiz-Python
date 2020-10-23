068cb6840da572da5032c7c5c52de5d1157484c8
Abdelhamid Khamis <abdelhameedkhamis@yahoo.com>
2020-10-23 08:33:47 +0200

    Dart Study - Create Cheat Sheet

---
+++ UdacityLab.py
@@ -0,0 +1,41 @@
+''' == {1} Variables and Containers(Data Types) == '''
+# -------------------------------------------------------------
+'''  {1.1} Variable Assignment  '''
+import math as m
+from math import sin,sinh
+"""
+The pythonic way to name variables is to use {snake_case} 
+{all lowercase letters} and {underscores} to separate words.
+my_height = 58
+my_lat = 40
+"""
+lst = "Name: abdelhameedkhamis@yahoo.com \n Abdelhamid\'s pass: RoTaNa_CiNeMa01032775729"
+name = "Abdelhamid"
+years = 25
+
+List = ['apple', 'banana', 'cherry','mango','guava']
+bikes = ['trek', 'redline', 'giant']
+users = ['val', 'bob', 'mia', 'ron', 'ned']
+
+alien = {'color': 'green', 'points': 5}
+alien_0 = {'color': 'green', 'points': 5}
+Dictionary = {
+  'brand': 'Ford',
+  'model': 'Mustang',
+  'year': 1964
+}
+
+Set = {'apple', 'banana', 'cherry'}
+Set1 = {'apple', 'banana', 'cherry'}
+Set2 = {'google', 'microsoft', 'apple'}
+
+Tuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5) # or 1,3,....
+Tuple1 = 'apple', 'banana', 'cherry'
+dimensions = (800, 600) 
+
+x7 = 1.2 + 8 + sin(90)  # Regular assignment
+x1,x2 = 2,3     # Multiple Assignment
+x1,x2 = x2,x1   # values swap
+x3 = x4 = 5     # same value assignment
+x1 += 1         # increment value by 1
+x2 *= 2         # x2 = x2 * 2
